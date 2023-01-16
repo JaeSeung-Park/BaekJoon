5dbbeffc9d190467c49cf14142ee2dce67887757
DESKTOP-9LP6PE9\pjs46 <wotmd36963@gmail.com>
2023-01-16 20:39:14 +0900

    #

---
+++ 2839-설탕배달(그리디).py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+c = 0
+while n >= 0:
+    if n % 5 == 0:
+        c += n // 5
+        print(c)
+        break
+    n -= 3
+    c += 1
+else:
+    print(-1)
